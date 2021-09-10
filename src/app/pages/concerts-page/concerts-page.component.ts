6d3e3800f3c0b4d658fb469ccdfec4f4627dfb64
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2021-09-10 16:22:11 +0200

    refactored styling

--- src/app/pages/concerts-page/concerts-page.component.ts
+++ src/app/pages/concerts-page/concerts-page.component.ts
@@ -17,30 +17,42 @@ export class ConcertsPageComponent implements OnInit {
 
   concerts: Concert[] = [];
   hasValues = false;
+  hasError = false;
 
   get showSpinner() {
-    return !this.hasValues;
+    return !this.hasValues && !this.hasError;
   }
 
   get showZeroConcertsMessage() {
-    return this.hasValues && this.concerts.length === 0;
+    return this.hasValues && this.concerts.length === 0 && !this.hasError;
   }
 
   get showConcerts() {
-    return this.hasValues && this.concerts.length > 0;
+    return this.hasValues && this.concerts.length > 0 && !this.hasError;
   }
 
   get spinnerDiameter(): number {
     return 60; // todo: make responsive
   }
 
+  get showErrorMessage() {
+    return this.hasError;
+  }
+
   constructor(private _concertService: ConcertService) {}
 
   ngOnInit(): void {
-    this._concertService.upcoming().subscribe((data) => {
-      this.concerts = data;
-      this.hasValues = true;
-    });
+    this._concertService.upcoming().subscribe(
+      (data) => {
+        this.concerts = data;
+        this.hasValues = true;
+      },
+      (error) => {
+        console.error('Cannot make request', error);
+        this.hasError = true;
+        console.log(this.showSpinner);
+      }
+    );
   }
 
   createDay(concert: Concert): string {
