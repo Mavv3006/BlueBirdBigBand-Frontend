f5216d36ada6fb465ed5915c5134baa56a633ccf
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2021-10-24 13:41:46 +0200

    added opening and closing band_submenu

--- src/app/layout/navbar/mobile/mobile-navbar.component.ts
+++ src/app/layout/navbar/mobile/mobile-navbar.component.ts
@@ -1,12 +1,12 @@
 import { SlidingMessageService } from './../../../services/sliding-message.service';
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-navbar',
   templateUrl: './mobile-navbar.component.html',
   styleUrls: ['./mobile-navbar.component.scss'],
 })
-export class MobileNavbarComponent {
+export class MobileNavbarComponent implements AfterViewInit {
   @ViewChild('band_container')
   band_container: ElementRef | undefined;
 
@@ -34,13 +34,38 @@ export class MobileNavbarComponent {
   isContactOpen = false;
 
   readonly news_pixel = 3 * 47;
-  readonly band_pixel = 2 * 47;
+  readonly band_pixel = 3 * 47;
   readonly contact_pixel = 2 * 47;
+
+  contact_submenu_native_element: HTMLElement | undefined;
 
   constructor(private messageService: SlidingMessageService) {}
 
+  ngAfterViewInit() {
+    this.contact_submenu_native_element = this.contact_submenu?.nativeElement;
+  }
+
   toggleNews() {}
-  toggleBand() {}
+  toggleBand() {
+    if (this.isBandOpen) {
+      this.contact_container_transform_pixel -= this.band_pixel;
+      this.contact_submenu_transform_pixel -= this.band_pixel;
+      this.login_container_transform_pixel -= this.band_pixel;
+      this.main_transform_pixel -= this.band_pixel;
+    } else {
+      this.contact_container_transform_pixel += this.band_pixel;
+      this.contact_submenu_transform_pixel += this.band_pixel;
+      this.login_container_transform_pixel += this.band_pixel;
+      this.main_transform_pixel += this.band_pixel;
+    }
+
+    this.updateContact();
+    this.updateLogin();
+    this.updateMain();
+
+    this.isBandOpen = !this.isBandOpen;
+  }
+
   toggleContact() {
     if (this.isContactOpen) {
       this.login_container_transform_pixel -= this.contact_pixel;
@@ -50,15 +75,24 @@ export class MobileNavbarComponent {
       this.main_transform_pixel += this.contact_pixel;
     }
 
-    (this.login_container?.nativeElement as HTMLElement).style.transform =
-      'translateY(' + this.login_container_transform_pixel + 'px)';
-    console.info(
-      (this.login_container?.nativeElement as HTMLElement).style.transform
-    );
-
+    this.updateContact();
+    this.updateLogin();
     this.updateMain();
 
     this.isContactOpen = !this.isContactOpen;
+  }
+
+  private updateContact() {
+    (this.contact_container?.nativeElement as HTMLElement).style.transform =
+      'translateY(' + this.contact_container_transform_pixel + 'px)';
+
+    (this.contact_submenu?.nativeElement as HTMLElement).style.transform =
+      'translateY(' + this.contact_submenu_transform_pixel + 'px)';
+  }
+
+  private updateLogin() {
+    (this.login_container?.nativeElement as HTMLElement).style.transform =
+      'translateY(' + this.login_container_transform_pixel + 'px)';
   }
 
   private updateMain() {
