18a62d91aa6da54a0dbea187634f239e9fc3b35a
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2022-01-06 20:59:32 +0100

    started with testing services

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,16 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { environment } from './../../environments/environment.prod';
+import { inject, TestBed, getTestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-import { AuthService } from './auth.service';
+import { AuthService, LoginResponse } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService],
+    });
+
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should log in', () => {
+    const mockResponse: LoginResponse = {
+      access_token: 'string',
+      token_type: 'string',
+      expires: {
+        in: 0,
+        at: 0,
+      },
+    };
+
+    service.login('test', 'test').subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(environment.urls.auth.login);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockResponse);
   });
 });
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { LocalStorageKey } from '../storage/local-storage-keys';
+
+export interface LoginResponse {
+  access_token: 'string';
+  token_type: 'string';
+  expires: {
+    in: number;
+    at: number;
+  };
+}
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +19,15 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   public login(username: string, password: string) {
-    return this.http.post(environment.urls.auth.login, '');
+    return this.http.post<LoginResponse>(environment.urls.auth.login, {
+      username: username,
+      password: password,
+    });
   }
 
-  public logout() {}
+  public logout() {
+    return this.http.get(environment.urls.auth.logout);
+  }
 
   public isLoggedIn(): boolean {
     // TODO: add check for format of item with LocalStorageKey.jwt key
